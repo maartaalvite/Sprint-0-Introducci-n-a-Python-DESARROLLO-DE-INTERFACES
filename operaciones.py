b926239bdd06f5596c085ef786c655093238ee90
mmarinoa <mmarinoa24@fpcoruna.afundacion.org>
2025-10-23 10:37:39 +0200

    Ejercicio 1 Desarrollo de Interfaces

---
+++ operaciones.py
@@ -0,0 +1,17 @@
+def suma(num1, num2):
+    return num1 + num2
+
+def resta(num1, num2):
+    return num1 - num2
+
+def multiplicacion(num1, num2):
+    return num1 * num2
+
+def division(num1, num2):
+    if num2 == 0:
+        return "Infinito"
+    else:
+        return num1 / num2
+    return num1 / num2
+
+if __name__ == '__main__':
